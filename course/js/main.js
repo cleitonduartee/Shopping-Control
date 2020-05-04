09d22a583988cc1edbabdc1d1106b2baa404a86a
CleitonDuarte <djcleitonduarte@hotmail.com>
2020-05-04 10:00:32 -0400

    melhorando validação...

--- course/js/main.js
+++ course/js/main.js
@@ -18,7 +18,7 @@ function setList(list){
     for(var key in list){
         table += `<tr>
         <td> ${formatDesc(list[key].description)}</td>
-        <td>${list[key].amount}</td>
+        <td> ${formatAmount(list[key].amount)}</td>
         <td>${formatValue(list[key].value)}</td>
         <td><button onclick="setUpdate(${key})"class="btn btn-light">Edit</button> <button onclick="deleteData(${key})"class="btn btn-light">Delete</button</td>                    
     </tr>`
@@ -31,6 +31,9 @@ function formatDesc(desc){
     str = str.charAt(0).toUpperCase() + str.slice(1); //Pega primeira letra(charAt) e colocar em Maiusculo(ToUpperCase) e pega o restante da string que está em minusculo com slice()
     return str
 }
+function formatAmount(amount){
+    return parseInt(amount);
+}
 function formatValue(value){
     var str = parseFloat(value).toFixed(2) + ""; //Transforma em Float, determina 2 casas decimais, e depois volta para string com + "";
     str = str.replace(".", ",") //troca o . por , 
@@ -38,14 +41,14 @@ function formatValue(value){
     return str
 }
 function addData(){
-    var item = lerDados()
-    if(validar(item)){
+    if(this.validation()){
+        return;
+    }
+        var item = lerDados()
         list.unshift({"description":item.desc , "amount":item.amount , "value":item.value})
         this.resetForm()
         setList(list);
-    }else{
-        alert("Preencha todos os Campos!")
-    }
+    
 }
 function lerDados(){
     var item = {}
@@ -54,12 +57,7 @@ function lerDados(){
     item.value = document.getElementById("value").value;
     return item;
 }
-function validar(item){
-    if(item.desc && item.amount && item.value){
-        return true
-    }
-    return false
-}
+
 function setUpdate(id){
     
     var obj = list[id];
@@ -80,8 +78,12 @@ function resetForm(){
     document.getElementById("btnUpdate").style.display = "none";
     document.getElementById("btnAdd").style.display = "inline-block";
     document.getElementById("inputIDUpadate").innerHTML = "";
+    document.getElementById("errors").style.display = "none"
 }
 function updateData(){
+    if(this.validation()){
+        return;
+    }
     var id = document.getElementById("idUpdate").value;
 
     var item = this.lerDados();
@@ -104,6 +106,47 @@ function deleteData(id){
         }
         setList(list)
 
+    }
+}
+function validation (){
+    var desc = document.getElementById("desc").value;
+    var amount = document.getElementById("amount").value;
+    var value = document.getElementById("value").value;
+
+    document.getElementById("errors").style.display = "none";
+    
+    var errors = ""
+    if(desc === ""){
+        errors += '<p>Digite uma Descrição</p>'
+    }
+    if(amount === ""){
+        errors += '<p>Digite a quantidade</p>'
+    }else if( amount != parseInt(amount)){
+        errors += '<p>Valor invalido no campo quantidade</p>'
+    }
+    if(value === ""){
+        errors += '<p>Digite o valor</p>'
+    }else if( value != parseFloat(value)){
+        errors += '<p>Valor invalido no campo valor</p>'
+    }
+
+    if(errors != ""){
+        
+        document.getElementById("errors").style.display = "block";
+        document.getElementById("errors").style.backgroundColor = "rgba(85,85,85, 0.3)";
+        document.getElementById("errors").style.color = "white";
+        document.getElementById("errors").style.padding = "10px";
+        document.getElementById("errors").style.margin = "10px auto";
+        document.getElementById("errors").style.borderRadius = "13px";
+        document.getElementById("errors").style.width = "30%";
+        document.getElementById("errors").style.textAlign = "center";
+        
+
+
+        document.getElementById("errors").innerHTML = "<h3>ERROR: </h3> "+errors;
+        return true;
+    }else{
+        return false;
     }
 }
 
